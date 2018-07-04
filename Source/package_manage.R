00544dbade2428f529acd0ba46a27b3fb1e7aa64
yunseop <blpomspo@gmail.com>
2018-07-04 15:35:22 +0900

    update forecast: pickInput, graph

--- Source/package_manage.R
+++ Source/package_manage.R
@@ -1,12 +1,13 @@
+# .libPaths('C:/Users/yunseop/Documents/R/win-library/3.5')
 # shiny
-bs.Library(c("shiny"))
+bs.Library(c("shiny", "shinyWidgets"))
 
 # data manipulation
-bs.Library(c('tidyverse', "data.table", "influxdbr"))
+bs.Library(c('tidyverse', "data.table", "influxdbr", 'xts'))
 
 # visualization
 bs.Library(c("dygraphs", "visNetwork", "networkD3", 'plotly',
-             'ggcorrplot'))
+             'ggcorrplot', 'ggplot2'))
 
 # machine learning
 bs.Library(c("prophet", "anomalize", 'lmtest'))
