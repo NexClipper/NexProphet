1ee7d3dee93ed8401c2a07cd07575a479beeac2c
yunseop <blpomspo@gmail.com>
2018-07-17 15:11:14 +0900

    ClientData test

--- forecast/app.R
+++ forecast/app.R
@@ -151,7 +151,9 @@ ui <- fluidPage(
           width = 6,
           br(),
           h4("Forecasting Statistics"),
-          br()
+          br(),
+          verbatimTextOutput("urlText"),
+          verbatimTextOutput("queryText")
         )
       )
       
@@ -163,6 +165,26 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
+  
+  output$urlText <- renderText({
+    paste(sep = "",
+          "protocol: ", session$clientData$url_protocol, "\n",
+          "hostname: ", session$clientData$url_hostname, "\n",
+          "pathname: ", session$clientData$url_pathname, "\n",
+          "port: ",     session$clientData$url_port,     "\n",
+          "search: ",   session$clientData$url_search,   "\n"
+    )
+  })
+  
+  # Parse the GET query string
+  output$queryText <- renderText({
+    query <- parseQueryString(session$clientData$url_search)
+    
+    # Return a string with key-value pairs
+    paste(names(query), query, sep = "=", collapse=", ")
+  })
+  
+  
   
   observeEvent(input$resource, {
     
