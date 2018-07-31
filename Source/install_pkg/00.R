91b764351bd5d83066511c4fb7952ee5d55e3dcb
yunseop <blpomspo@gmail.com>
2018-07-31 16:38:29 +0900

    add install package script

---
+++ Source/install_pkg/00.R
@@ -0,0 +1,29 @@
+bs.Library <- function(pkg, add = T) {
+  
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  
+  if (length(new.pkg))
+    
+    install.packages(new.pkg, dependencies = T)
+  
+  if (add == TRUE)
+    
+    sapply(pkg, require, character.only = TRUE)
+  
+}
+
+
+# shiny
+bs.Library(c("shiny", "shinyWidgets", 'shinyjs', 'shinycssloaders'))
+
+
+# data manipulation
+bs.Library(c('tidyverse', "data.table", "influxdbr", 'xts'))
+
+
+# visualization
+bs.Library(c("dygraphs", 'd3heatmap'))
+
+
+# machine learning
+bs.Library(c("prophet", 'lmtest'))
