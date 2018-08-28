d7fd8e36995fdfc74403cb2dfa46ba91ec024f4f
yunseop <blpomspo@gmail.com>
2018-08-28 15:47:40 +0900

    .

--- disk/app_func.R
+++ disk/app_func.R
@@ -53,9 +53,6 @@ CUT <- internal$cut
 CONN <- influx_connection(host = INFLUX_HOST,
                           port = INFLUX_PORT)
 
-AGENT_ID <- get_agent_id()
-
-create_mysql_table()
 #----
 
 
@@ -88,6 +85,58 @@ get_agent_id <- function(id = ID,
   return(res$agent_id)
   
 }
+
+
+create_mysql_table <- function(user = MYSQL_USER,
+                               password = MYSQL_PASSWORD,
+                               dbname = MYSQL_DBNAME,
+                               host = MYSQL_HOST,
+                               port = MYSQL_PORT) {
+  
+  con <- dbConnect(MySQL(), 
+                   user = user, 
+                   password = password,
+                   dbname = dbname,
+                   host = host, 
+                   port = port)
+  
+  ListTables <- dbListTables(con)
+  
+  if (!('monitoring_disk' %in% ListTables)) {
+    
+    dbGetQuery(con,
+               "CREATE TABLE `monitoring_disk` (
+               `id` int(11) NOT NULL,
+               `agent_id` int(11) NOT NULL,
+               `resource` varchar(10) CHARACTER SET utf8 NOT NULL,
+               `mount` varchar(1000) CHARACTER SET utf8 NOT NULL,
+               `current_time` datetime NOT NULL,
+               `DFT` datetime,
+               `predicted` float NOT NULL,
+               `alertYN` tinyint(1) NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;")
+    
+    dbGetQuery(con,
+               "ALTER TABLE `monitoring_disk` ADD PRIMARY KEY (`id`);")
+    
+    dbGetQuery(con,
+               "ALTER TABLE `monitoring_disk` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;")
+    
+    print('Create monitoring_disk table')
+    
+    dbCommit(con)
+    
+  }
+  
+  dbDisconnect(con)
+  
+}
+
+
+AGENT_ID <- get_agent_id()
+
+
+create_mysql_table()
 
 
 load_disk_used_percent <- function(agent_id = AGENT_ID,
@@ -362,48 +411,3 @@ send_slack <- function() {
   
 }
 
-
-create_mysql_table <- function(user = MYSQL_USER,
-                               password = MYSQL_PASSWORD,
-                               dbname = MYSQL_DBNAME,
-                               host = MYSQL_HOST,
-                               port = MYSQL_PORT) {
-  
-  con <- dbConnect(MySQL(), 
-                   user = user, 
-                   password = password,
-                   dbname = dbname,
-                   host = host, 
-                   port = port)
-  
-  ListTables <- dbListTables(con)
-  
-  if (!('monitoring_disk' %in% ListTables)) {
-    
-    dbGetQuery(con,
-               "CREATE TABLE `monitoring_disk` (
-               `id` int(11) NOT NULL,
-               `agent_id` int(11) NOT NULL,
-               `resource` varchar(10) CHARACTER SET utf8 NOT NULL,
-               `mount` varchar(1000) CHARACTER SET utf8 NOT NULL,
-               `current_time` datetime NOT NULL,
-               `DFT` datetime,
-               `predicted` float NOT NULL,
-               `alertYN` tinyint(1) NOT NULL
-    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;")
-  
-    dbGetQuery(con,
-               "ALTER TABLE `monitoring_disk` ADD PRIMARY KEY (`id`);")
-    
-    dbGetQuery(con,
-               "ALTER TABLE `monitoring_disk` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;")
-    
-    print('Create monitoring_disk table')
-    
-    dbCommit(con)
-    
-  }
-  
-  dbDisconnect(con)
-  
-}
