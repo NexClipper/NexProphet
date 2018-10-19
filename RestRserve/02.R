bf32ed59ad0beae32388158a6d3eb5ef2bb0711f
yunseop <blpomspo@gmail.com>
2018-10-19 11:03:20 +0900

    rmysql install

---
+++ RestRserve/02.R
@@ -0,0 +1,18 @@
+# Check library existance
+bs.Library <- function(pkg, add = T) {
+  
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  
+  if (length(new.pkg))
+    
+    install.packages(new.pkg, dependencies = T)
+  
+  if (add == TRUE)
+    
+    sapply(pkg, require, character.only = TRUE)
+  
+}
+
+
+# data manipulation
+bs.Library(c('RMySQL'))
